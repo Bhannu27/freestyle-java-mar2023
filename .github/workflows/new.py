96c2b7f030331f0f39a637a69b41baa1960f3762
Bhannu27 <100187170+Bhannu27@users.noreply.github.com>
2024-02-06 11:09:41 +0530

    Update new.py

--- .github/workflows/new.py
+++ .github/workflows/new.py
@@ -2,35 +2,27 @@ import csv
 import os
 
 csv_file_path = os.environ['csv_path']
- 
+
 with open(csv_file_path, 'r') as file:
     csv_reader = csv.DictReader(file)
-    for row in csv_reader:
-        branch_name = row['BranchName']
-        build_number = row['BuildNumber']
-        workflow_type = row['WorkflowType']
-        instance_count = row['InstanceCount']
-     #   instance_name = row['InstanceName']
-        shared_module_instance_name = row['SharedModuleInstanceName']
+    row = next(csv_reader)
 
-      #  print(f" '{instance_name}' from branch {branch_name}")
-       # instance_name = f'"{instance_name}"'
-        result = {
-            'branch_name': branch_name,
-            'build_number': build_number,
-            'workflow_type': workflow_type,
-            'instance_count': instance_count,
-   #         'instance_name': instance_name,
-            'shared_module_instance_name': shared_module_instance_name
-        }
-        print(result)
+    # Extract values from the row
+    branch_name = row.get('BranchName', '')
+    build_number = row.get('BuildNumber', '')
+    workflow_type = row.get('WorkflowType', '')
+    instance_name = row.get('InstanceName', '')
+    instance_count = row.get('InstanceCount', '')
+    shared_module_instance_name = row.get('SharedModuleInstanceName', '')
 
+    # Print csv_values
+    csv_values = ",".join([branch_name, build_number, workflow_type, instance_name, instance_count, shared_module_instance_name])
+    print("CSV Values:", csv_values)
 
-  #  row = next(csv_reader)
- 
-  #  os.environ['BRANCH_NAME'] = row['BranchName']
-  #  os.environ['BUILD_NUMBER'] = row['BuildNumber']
-  # os.environ['WORKFLOW_TYPE'] = row['WorkflowType']
-  #  os.environ['INSTANCE_COUNT'] = row['InstanceCount']
-  #  os.environ['INSTANCE_NAME'] = row['InstanceName']
-  #  os.environ['SHARED_MODULE_INSTANCE_NAME'] = row['SharedModuleInstanceName']
+    # Print variables for verification
+    print("Branch Name:", branch_name)
+    print("Build Number:", build_number)
+    print("Workflow Type:", workflow_type)
+    print("Instance Name:", instance_name)
+    print("Instance Count:", instance_count)
+    print("Shared Module:", shared_module_instance_name)
