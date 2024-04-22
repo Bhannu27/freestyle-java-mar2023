7e3746cde1c255f3e86b16d90247c795366250e4
Bhannu27 <100187170+Bhannu27@users.noreply.github.com>
2024-04-22 19:47:05 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,8 +2,8 @@ public class HelloWorld {
 
    public static void main(String[] args) {
 
-      System.out.print("how are you, I'm doing good!!");
-      System.out.print("great");
+    //  System.out.print("how are you, I'm doing good!!");
+    //  System.out.print("great");
 
    }
 
